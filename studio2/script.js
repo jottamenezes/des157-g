95a850f3a48b03906549e417c7c15091844f680d
Jean Menezes <jottamenezes@gmail.com>
2019-02-11 01:46:43 -0800

    beautified text

--- studio2/script.js
+++ studio2/script.js
@@ -13,12 +13,12 @@ This script is part of the assignment studio 2 for
 var img1 = document.querySelector('#img1');
 var closeButton = document.querySelector('#closeButton');
 
-img1.addEventListener('click', function () {
+img1.addEventListener('click', function() {
   console.log('mousedown on button');
-  img1.style.opacity="0.07";
-  txt1.style.display= "block";
-    closeButton.style.display="block";
-    closeButton.style.left = "280px";
+  img1.style.opacity = "0.07";
+  txt1.style.display = "block";
+  closeButton.style.display = "block";
+  closeButton.style.left = "280px";
   closeButton.style.top = "80px";
 
 });
@@ -26,12 +26,12 @@ img1.addEventListener('click', function () {
 
 var img2 = document.querySelector('#img2');
 
-img2.addEventListener('click', function () {
+img2.addEventListener('click', function() {
   console.log('mousedown on button');
-  img2.style.opacity="0.07";
-  txt2.style.display= "block";
-  closeButton.style.display="block";
-    closeButton.style.left = "280px";
+  img2.style.opacity = "0.07";
+  txt2.style.display = "block";
+  closeButton.style.display = "block";
+  closeButton.style.left = "280px";
   closeButton.style.top = "500px";
 
 });
@@ -39,10 +39,10 @@ img2.addEventListener('click', function () {
 
 var close = document.getElementById('closeButton');
 
-close.addEventListener('click', function(){
-  close.style.display="none";
-  img1.style.opacity="1";
-  img2.style.opacity="1";
-  txt1.style.display= "none";
-  txt2.style.display= "none";
+close.addEventListener('click', function() {
+  close.style.display = "none";
+  img1.style.opacity = "1";
+  img2.style.opacity = "1";
+  txt1.style.display = "none";
+  txt2.style.display = "none";
 })
