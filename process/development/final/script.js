4b00d9d02480d3382a73cfbafa735f1be71439b1
Jean Menezes <jottamenezes@gmail.com>
2019-03-13 06:42:40 -0700

    added 3rd card

--- process/development/final/script.js
+++ process/development/final/script.js
@@ -34,9 +34,10 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 var hoverCard1 = document.querySelector('#aesthetics');
 var hoverCard2 = document.querySelector('#metaphysics');
+var hoverCard3 = document.querySelector('#epistemology');
 var img1 = document.querySelector('#imgCard1');
 var img2 = document.querySelector('#imgCard2');
-
+var img3= document.querySelector('#imgCard3');
 
 var textBox = document.querySelector('.textBox');
 var sectionTitle = document.querySelector('.cardTitle');
@@ -114,6 +115,43 @@ hoverCard2.addEventListener('click', function() {
 
 
     img2.src = "images/metaphysicsBG.png"
+    textBox.style.display = "none";
 
+});
+
+
+
+
+//CARD 2
+
+img3.addEventListener('mouseover', function() {
+  sectionTitle.innerHTML = "Epistemology";
+  sectionDescription.innerHTML = "The greatest questions we dared to ask, what is to know? How can we know that we do so?";
+
+});
+
+
+hoverCard3.addEventListener('mouseover', function() {
+
+  img3.src = "images/epistemologyBGHover.png";
+  img3.style.opacity = 1;
+  textBox.style.display = "block";
+  textBox.style.left = "2200px";
+  textBox.style.opacity = "1";
+
+});
+
+hoverCard3.addEventListener('mouseout', function() {
+  textBox.style.left = "2170px",
+    textBox.style.opacity = "0",
+    img3.src = "images/epistemologyBG.png"
+  img3.style.opacity = 0.7;
+});
+
+hoverCard3.addEventListener('click', function() {
+
+
+  img3.src = "images/epistemologyBG.png"
+    textBox.style.display = "none";
 
 });
